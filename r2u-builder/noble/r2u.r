bf10a60aa11b74760f125130b2a96b41afd7c521
Dirk Eddelbuettel <edd@debian.org>
2026-01-20 07:44:58 -0600

    Run rustup to get (build-time current) cargo / rustc, update r2u.r

--- r2u-builder/noble/r2u.r
+++ r2u-builder/noble/r2u.r
@@ -62,14 +62,17 @@ if (is.finite(match(opt$CMD, "build"))) {
                          opt$dryrun)
 
 } else if (is.finite(match(opt$CMD, "last"))) {
-    D <- RcppSimdJson::fload("https://p3m.dev/__api__/sources/1/transactions")
+    # NB now dead too:  D <- RcppSimdJson::fload("https://p3m.dev/__api__/sources/1/transactions")
     #ts <- anytime::utctime(D[1,"completed"])
     #dh <- as.numeric(difftime(Sys.time(), ts, units="hours"))
     #un <- "days"
     #if (dh <= 3) un <- "mins" else if (dh < 25) un <- "hours"
     #cat("P3M/PPM/RSPM last updated", format(round(difftime(Sys.time(), ts, units=un),1)), "ago\n")
     ## no longer includes hours :-/
-    ld <- anytime::utcdate(D[1,"completed"])
+    #ld <- anytime::utcdate(D[1,"completed"])
+    ## also:  hit given yyyy-mm-dd in eg https://packagemanager.posit.co/cran/2025-04-22/src/contrib/PACKAGES
+    ##        and then check success / failure -- might be lighter than downloading the whole blob
+    ld <- as.Date(max(RcppSimdJson::fload("https://p3m.dev/__api__/repos/cran/transaction-dates")$alias))
     cat("P3M/PPM/RSPM last updated", as.integer(Sys.Date()) - as.integer(ld), "days ago on", format(ld), "\n")
 
 } else if (is.finite(match(opt$CMD, "count"))) {
